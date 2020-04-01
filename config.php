4ac9bbd916aa37120a4c0fecb087d34b2adead21
Lauren Blonien <blonien@usc.edu>
2020-04-01 15:46:59 -0700

    Fixing DB and Updating / Switching to php files

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-	define('DB_HOST', '460.itpwebdev.com/~colab/');
+	define('DB_HOST', 'localhost');
 	define('DB_USER', 'colab_lauren');
 	define('DB_PASS', 'TeamAlpha$2020');
 	define('DB_NAME', 'colab_user_db');
