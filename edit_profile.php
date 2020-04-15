dff446b7a8e545f745f98dd2e9c9e775b6d31a94
Lauren Blonien <blonien@usc.edu>
2020-04-15 15:13:56 -0700

    Populating the drop downs

--- edit_profile.php
+++ edit_profile.php
@@ -1,6 +1,7 @@
 <?php
 	require "config.php";
 
+// Establish MySQL Connection.
 	$mysqli = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
 	if ( $mysqli->connect_errno ) {
@@ -8,8 +9,9 @@
 		exit();
 	}
 
-	$sql_users = "SELECT * FROM users;";
+	$mysqli->set_charset('utf8');
 
+	$sql_users = "SELECT * FROM users;";
 	$results_users = $mysqli->query( $sql_users );
 
 	if ( $results_users == false ) {
@@ -18,8 +20,52 @@
 		exit();
 	}
 
-	// var_dump($results_users);
+// Schools:
+	$sql_school = "SELECT * FROM school;";
+	$results_school = $mysqli->query($sql_school);
+	if ( $results_school == false ) {
+		echo $mysqli->error;
+		$mysqli->close();
+		exit();
+	}
 
+// Yeah in School:
+	$sql_school_year = "SELECT * FROM school_year;";
+	$results_school_year = $mysqli->query($sql_school_year);
+	if ( $results_school_year== false ) {
+		echo $mysqli->error;
+		$mysqli->close();
+		exit();
+	}
+
+// Major:
+	$sql_major = "SELECT * FROM major;";
+	$results_major = $mysqli->query($sql_major);
+	if ( $results_major == false ) {
+		echo $mysqli->error;
+		$mysqli->close();
+		exit();
+	}
+
+// Minor:
+	$sql_minor = "SELECT * FROM minor;";
+	$results_minor = $mysqli->query($sql_minor);
+	if ( $results_minor == false ) {
+		echo $mysqli->error;
+		$mysqli->close();
+		exit();
+	}
+
+// Industry:
+	$sql_industry = "SELECT * FROM industry;";
+	$results_industry = $mysqli->query($sql_industry);
+	if ( $results_industry == false ) {
+		echo $mysqli->error;
+		$mysqli->close();
+		exit();
+	}
+
+// Close DB Connection
 	$mysqli->close();
 ?>
 <!DOCTYPE html>
@@ -57,7 +103,7 @@
 					<span id="email" style="color:dodgerblue;">euminlee@usc.edu</span><br><br>
 				
 				<!-- edit profile form -->
-					<form id="signup-form">
+					<form id="signup-form" method="POST">
 						<div class="form-group">
 							<label class="card-subtitle mb-2 text-muted" for="bio"><h4 class="label">BIO</h4></label>
 							<textarea class="form-control" id="bio-id" name="bio"></textarea>
@@ -71,8 +117,19 @@
 						<div class="form-group">
 							<label class="card-subtitle mb-2 text-muted" for="email"><h4 class="label">SCHOOL</h4></label>
 							<select name="school" id="school-id" class="form-control">
-								<option value="" selected>-- Select One --</option>
-								<option value="<?php echo $row['label_id']; ?>"></option>
+								<option value="" selected disabled>-- Select One --</option>
+									<?php while( $row = $results_school->fetch_assoc() ): ?>
+
+									<?php if ( $row['school_id'] == $row_users['school_id'] ) : ?>
+
+								<option value="<?php echo $row['school_id']; ?>" selected>
+									<?php echo $row['school_name']; ?></option>
+									<?php else : ?>
+
+								<option value="<?php echo $row['school_id']; ?>">
+									<?php echo $row['school_name']; ?></option>
+									<?php endif; ?>
+									<?php endwhile; ?>
 							</select>
 						</div>
 
@@ -80,26 +137,57 @@
 							<label class="card-subtitle mb-2 text-muted" for="email"><h4 class="label">YEAR IN SCHOOL</h4></label>
 
 							<select name="school-year" id="school-id" class="form-control">
-								<option value="" selected>-- Select One --</option>
-								<option value="<?php echo $row['label_id']; ?>"></option>
+								<option value="" selected disabled>-- Select One --</option>
+									<?php while( $row = $results_school_year->fetch_assoc() ): ?>
+
+									<?php if ( $row['school_year_id'] == $row_users['school_year_id'] ) : ?>
+
+								<option value="<?php echo $row['school_year_id']; ?>" selected>
+									<?php echo $row['year']; ?></option>
+									<?php else : ?>
+
+								<option value="<?php echo $row['school_year_id']; ?>">
+									<?php echo $row['year']; ?></option>
+									<?php endif; ?>
+									<?php endwhile; ?>
 							</select>
 						</div>
 
 						<div class="form-group">
 							<label class="card-subtitle mb-2 text-muted" for="major"><h4 class="label">MAJOR</h4></label>
 							<select name="major" id="school-id" class="form-control">
-								<option value="" selected>-- Select One --</option>
-								<option value="<?php echo $row['label_id']; ?>">
-								</option>
+								<option value="" selected disabled>-- Select One --</option>
+									<?php while( $row = $results_major->fetch_assoc() ): ?>
+
+									<?php if ( $row['major_id'] == $row_users['major_id'] ) : ?>
+
+								<option value="<?php echo $row['major_id']; ?>" selected>
+									<?php echo $row['major']; ?></option>
+									<?php else : ?>
+
+								<option value="<?php echo $row['major_id']; ?>">
+									<?php echo $row['major']; ?></option>
+									<?php endif; ?>
+									<?php endwhile; ?>
 							</select>
 						</div>
 
 						<div class="form-group">
 							<label class="card-subtitle mb-2 text-muted" for="minor"><h4 class="label">MINOR</h4></label>
 							<select name="minor" id="school-id" class="form-control">
-								<option value="" selected>-- Select One --</option>
-								<option value="<?php echo $row['label_id']; ?>">
-								</option>
+								<option value="" selected disabled>-- Select One --</option>
+									<?php while( $row = $results_minor->fetch_assoc() ): ?>
+
+									<?php if ( $row['minor_id'] == $row_users['minor_id'] ) : ?>
+
+								<option value="<?php echo $row['minor_id']; ?>" selected>
+									<?php echo $row['minor']; ?></option>
+									<?php else : ?>
+
+								<option value="<?php echo $row['minor_id']; ?>">
+									<?php echo $row['minor']; ?></option>
+									<?php endif; ?>
+									<?php endwhile; ?>
 							</select>
 						</div>
 						
@@ -116,9 +204,19 @@
 						<div class="form-group">
 							<label class="card-subtitle mb-2 text-muted" for="industry"><h4 class="label">INDUSTRY</h4></label>
 							<select name="industry" id="school-id" class="form-control">
-								<option value="" selected>-- Select One --</option>
-								<option value="<?php echo $row['label_id']; ?>">
-								</option>
+								<option value="" selected disabled>-- Select One --</option>
+									<?php while( $row = $results_industry->fetch_assoc() ): ?>
+
+									<?php if ( $row['industry_id'] == $row_users['industry_id'] ) : ?>
+
+								<option value="<?php echo $row['industry_id']; ?>" selected>
+									<?php echo $row['industry']; ?></option>
+									<?php else : ?>
+
+								<option value="<?php echo $row['industry_id']; ?>">
+									<?php echo $row['industry']; ?></option>
+									<?php endif; ?>
+									<?php endwhile; ?>
 							</select>
 						</div>
 
