bacbfe3dfb13a36c0212c4690866d43d61650bc4
Teagan Ampe <ampe@usc.edu>
2020-04-22 00:14:37 -0700

    more testing

--- index.php
+++ index.php
@@ -97,7 +97,7 @@
 	<meta charset=“utf-8”>
 
 	<!-- Hotjar Tracking Code for https://460.itpwebdev.com/~colab/ -->
-<script>
+<!-- <script>
     (function(h,o,t,j,a,r){
         h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
         h._hjSettings={hjid:1758353,hjsv:6};
@@ -106,7 +106,7 @@
         r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
         a.appendChild(r);
     })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-</script>
+</script> -->
 </head>
 <body>
 
