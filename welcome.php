584afe98e72f86b0cb2c1c1ce7c992a5e3bc8144
Teagan Ampe <ampe@usc.edu>
2020-04-22 17:32:46 -0700

    added seo meta tag

--- welcome.php
+++ welcome.php
@@ -16,6 +16,8 @@
 <head>
 
     <meta charset="utf-8">
+    <!-- meta tag for SEO -->
+    <meta  name="description" content="Join and search coLab's community of college students to find your next collaborator or mentor.">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
     
